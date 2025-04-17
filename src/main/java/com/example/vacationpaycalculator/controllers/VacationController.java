12734204efaac79234bdff0b16db01ebf9fa6ea2
Botkin Maxim <68293738+MaximBotkin@users.noreply.github.com>
2025-04-17 22:19:42 +0300

    add swagger

--- src/main/java/com/example/vacationpaycalculator/controllers/VacationController.java
+++ src/main/java/com/example/vacationpaycalculator/controllers/VacationController.java
@@ -1,5 +1,10 @@
 package com.example.vacationpaycalculator.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 import com.example.vacationpaycalculator.model.Vacation;
 import com.example.vacationpaycalculator.service.VacationService;
@@ -15,17 +20,29 @@ import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/calculate")
+@Tag(name = "Калькулятор отпускных", description = "API для расчета суммы отпускных")
 public class VacationController {
 
     @Autowired
     private VacationService vacationService;
 
+    @Operation(
+            summary = "Рассчитать отпускные",
+            description = "Возвращает сумму отпускных на основе средней зарплаты и количества дней",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Успешный расчет"),
+                    @ApiResponse(responseCode = "400", description = "Неверные параметры запроса")
+            }
+    )
+
     @GetMapping
-    public ResponseEntity<String> calculateVacation(@RequestParam int averageSalary,
-                                                    @RequestParam int daysNumber,
-                                                    @RequestParam(required = false)
-                                                    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-                                                        LocalDate startDate) {
+    public ResponseEntity<String> calculateVacation(
+            @Parameter(description = "Средняя зарплата (руб)", example = "100000")
+            @RequestParam int averageSalary,
+            @Parameter(description = "Количество дней отпуска", example = "14")
+            @RequestParam int daysNumber,
+            @Parameter(description = "Дата начала отпуска (опционально)", example = "2024-10-01")
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate) {
         Vacation vacation = new Vacation();
         vacation.setAverageSalary(averageSalary);
         vacation.setDaysNumber(daysNumber);
